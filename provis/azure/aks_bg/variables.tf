873a0e580cf2e03db8bbd2425802d123992a9d24
kjh1234 <kjh1234@gmail.com>
2021-03-17 15:04:36 +0900

    Update variables.tf

--- provis/azure/aks_bg/variables.tf
+++ provis/azure/aks_bg/variables.tf
@@ -18,7 +18,7 @@ variable "tenant_id" {
 #     resource variables     #
 variable "app_resource_group_name" {
   description = "Resource Group name, must be lowercase alphanumeric with hyphens as its used as domain_name_label as well"
-  default     = "vmss-tf-test"
+  default     = "aks-tf-jenkins"
 }
 
 variable "location" {
@@ -26,32 +26,12 @@ variable "location" {
   default     = "koreacentral"
 }
 
-variable "admin_id" {
-    description = "Default password for azureuser"
-    default = "azureuser"
+variable "cluster_name" {
+    default = "aks-bg-cluster"
 }
 
-variable "admin_password" {
-    description = "Default password for azureuser"
-    default = "dlatl!1234"
+variable "node_count" {
+    default = "2"
 }
 
-variable "frontend_port" {
-    description = "The frontend port of the external Load Balancer"
-    default     = 80
-}
 
-variable "application_port" {
-    description = "The backend port where the application can be accessed"
-    default     = 8080
-}
-
-variable "image_resource_group_name" {
-  description = "Resource Group name for packer images"
-  default     = "vmss-bg-image-gr"
-}
-
-variable "image_name" {
-  description = "The name of OS image to use"
-  default     = "tomcat-7"
-}
