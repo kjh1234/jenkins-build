b3acb2db2041e8827acae20baba097d97f61169d
kjh1234 <kjh1234@gmail.com>
2021-03-17 16:55:55 +0900

    Update azure_aks_bg.groovy

--- provis/azure_aks_bg.groovy
+++ provis/azure_aks_bg.groovy
@@ -79,7 +79,8 @@ pipeline {
         // Apply the plan
         sh """
           companion_rg="MC_${RESOURCE_GROUP}_${AKS_NAME}_${LOCATIONS}"
-          kubeconfig="$\(mktemp\)"
+          
+          kubeconfig="mktemp"
           
         """
         /*
