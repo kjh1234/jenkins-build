f3ed32ab98c9bee6f938b73887c5df8c8295d65d
kjh1234 <kjh1234@gmail.com>
2021-03-17 18:01:04 +0900

    Update azure_aks_bg.groovy

--- provis/azure_aks_bg.groovy
+++ provis/azure_aks_bg.groovy
@@ -7,7 +7,6 @@ pipeline {
         checkout scm
       }
     }
-    /*
     stage('Terraform init'){
       steps {
         // Initialize the plan
@@ -65,8 +64,7 @@ pipeline {
         }
       }
     }
-    */
-
+/*
     stage('K8s Create Service'){
       steps {
         // Get the VM image ID for the VMSS
@@ -121,53 +119,9 @@ pipeline {
         assign_dns todoapp-test-blue "aks-todoapp-blue-dns"
         assign_dns todoapp-test-green "aks-todoapp-green-dns"
         """
-        /*
-        sh  """
-          companion_rg="MC_${RESOURCE_GROUP}_${AKS_NAME}_${LOCATIONS}"
-          kubeconfig="$(mktemp)"
-          
-          echo "Fetch AKS credentials to $kubeconfig"
-          az aks get-credentials -g "${RESOURCE_GROUP}" -n "${AKS_NAME}" --admin --file "$kubeconfig"
-          
-          echo "Apply Service"
-          kubectl apply -f "${workspace}/provis/azure/aks_bg/service-green.yml" --kubeconfig "$kubeconfig"
-          kubectl apply -f "${workspace}/provis/azure/aks_bg/test-endpoint-blue.yml" --kubeconfig "$kubeconfig"
-          kubectl apply -f "${workspace}/provis/azure/aks_bg/test-endpoint-green.yml" --kubeconfig "$kubeconfig"
-          
-          
-          function assign_dns {
-            service="$1"
-            dns_name="$2"
-            IP=
-            while true; do
-                echo "Waiting external IP for $service..."
-                IP="$(kubectl get service "$service" --kubeconfig "$kubeconfig" | tail -n +2 | awk '{print $4}' | grep -v '<')"
-                if [[ "$?" == 0 && -n "$IP" ]]; then
-                    echo "Service $service public IP: $IP"
-                    break
-                fi
-                sleep 10
-            done
-
-            public_ip="$(az network public-ip list -g "$companion_rg" --query "[?ipAddress==\`$IP\`] | [0].id" -o tsv)"
-            if [[ -z "$public_ip" ]]; then
-                echo "Cannot find public IP resource ID for '$service' in companion resource group '$companion_rg'" >&2
-                exit 1
-            fi
-
-            echo "Assign DNS name '$dns_name' for '$service'"
-            az network public-ip update --dns-name "$dns_name" --ids "$public_ip"
-            [[ $? != 0 ]] && exit 1
-        }
-
-        assign_dns todoapp-service "aks-todoapp-dns"
-        assign_dns todoapp-test-blue "aks-todoapp-blue-dns"
-        assign_dns todoapp-test-green "aks-todoapp-green-dns"
-        """
-        */
       }
     }
-
+*/
     stage('Post-clean') {
       steps {
         sh '''
