6d7ad0abbf70e1b6f5816552c6a20b76cdd38d77
kjh1234 <kjh1234@gmail.com>
2021-03-17 14:01:59 +0900

    Update azure_vmss_bg.groovy

--- provis/azure_vmss_bg.groovy
+++ provis/azure_vmss_bg.groovy
@@ -24,9 +24,13 @@ pipeline {
         withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
           sh """
             az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
+            az account set --subscription $AZURE_SUBSCRIPTION_ID
+            
+            export ARM_CLIENT_ID="$AZURE_CLIENT_ID"
+            export ARM_CLIENT_SECRET="$AZURE_CLIENT_SECRET"
+            export ARM_SUBSCRIPTION_ID="$AZURE_SUBSCRIPTION_ID"
+            export ARM_TENANT_ID="$AZURE_TENANT_ID"
           """
-          
-          sh (script:"cd ${workspace}/provis/azure/vmss_bg && terraform plan -out=tfplan -input=false -var 'app_resource_group_name=vmss-tf-jenkins'")
         }
 
         /*
@@ -36,6 +40,8 @@ pipeline {
         */
 
         // sh (script:"cd ${workspace}/provis/azure/vmss_bg && terraform plan -out=tfplan -input=false -var 'terraform_resource_group='$vmss_rg -var 'terraform_vmss_name='$vmss_name -var 'terraform_azure_region='$location -var 'terraform_image_id='$image_id")
+          
+        sh (script:"cd ${workspace}/provis/azure/vmss_bg && terraform plan -out=tfplan -input=false -var 'app_resource_group_name=vmss-tf-jenkins'")
         
       }
     }
@@ -54,5 +60,6 @@ pipeline {
   }
   environment {
     INNO_AZURE_CREDENTIALS = 'INNO_AZURE_CREDENTIALS'
+    AZURE_SUBSCRIPTION_ID = credentials('AZURE_SUBSCRIPTION_ID')
   }
 }
