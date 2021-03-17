d96b19b86f8e16c3dd02704a7fcd04de445fddb5
kjh1234 <kjh1234@gmail.com>
2021-03-17 14:11:07 +0900

    Update azure_vmss_bg.groovy

--- provis/azure_vmss_bg.groovy
+++ provis/azure_vmss_bg.groovy
@@ -25,11 +25,11 @@ pipeline {
           sh """
             az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
             az account set --subscription $AZURE_SUBSCRIPTION_ID
-            
-            export ARM_CLIENT_ID="$AZURE_CLIENT_ID"
-            export ARM_CLIENT_SECRET="$AZURE_CLIENT_SECRET"
-            export ARM_SUBSCRIPTION_ID="$AZURE_SUBSCRIPTION_ID"
-            export ARM_TENANT_ID="$AZURE_TENANT_ID"
+           
+            export ARM_CLIENT_ID="${AZURE_CLIENT_ID}"
+            export ARM_CLIENT_SECRET="${AZURE_CLIENT_SECRET}"
+            export ARM_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID}"
+            export ARM_TENANT_ID="${AZURE_TENANT_ID}"
           """
         }
 
