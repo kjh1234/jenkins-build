f982e6eb0818dd495ae6ad5499033b88ed84af60
kjh1234 <kjh1234@gmail.com>
2021-03-17 13:43:12 +0900

    Update azure_vmss_bg.groovy

--- provis/azure_vmss_bg.groovy
+++ provis/azure_vmss_bg.groovy
@@ -35,7 +35,7 @@ pipeline {
 
         // sh (script:"cd ${workspace}/provis/azure/vmss_bg && terraform plan -out=tfplan -input=false -var 'terraform_resource_group='$vmss_rg -var 'terraform_vmss_name='$vmss_name -var 'terraform_azure_region='$location -var 'terraform_image_id='$image_id")
 
-        sh (script:"cd ${workspace}/provis/azure/vmss_bg && terraform plan -out=tfplan -input=false -var 'terraform_resource_group=vmss-tf-jenkins'")
+        sh (script:"cd ${workspace}/provis/azure/vmss_bg && terraform plan -out=tfplan -input=false -var 'app_resource_group_name=vmss-tf-jenkins'")
 
       }
     }
