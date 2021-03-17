92b4829eb10d98bd8c2803e4637ea6d8cadb9f87
kjh1234 <kjh1234@gmail.com>
2021-03-17 16:09:13 +0900

    Update main.tf

--- provis/azure/aks_bg/main.tf
+++ provis/azure/aks_bg/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
     default_node_pool {
         name            = "agentpool"
-        node_count      = ${var.node_count}
+        node_count      = "${var.node_count}"
         vm_size         = "Standard_D2_v2"
     }
 
