6b26dc3f3d45437ddf200c17d68862b68db6df99
kjh1234 <kjh1234@gmail.com>
2021-03-17 15:03:09 +0900

    Update main.tf

--- provis/azure/aks_bg/main.tf
+++ provis/azure/aks_bg/main.tf
@@ -5,3 +5,44 @@ provider "azurerm" {
   client_secret = "${var.client_secret}"
   tenant_id = "${var.tenant_id}"
 }
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.app_resource_group_name}"
+  location = "${var.location}"
+}
+
+az aks create -g "$resource_group" -n "$aks_name" --ssh-key-value "$public_key" --node-count 2 --kubernetes-version 1.18.14
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+    name                = "${var.cluster_name}"
+    location            = "${azurerm_resource_group.main.location}"
+    resource_group_name = "${azurerm_resource_group.main.name}"
+
+    linux_profile {
+        admin_username = "ubuntu"
+
+        ssh_key {
+            key_data = file(${var.public_key})
+        }
+    }
+
+    default_node_pool {
+        name            = "agentpool"
+        node_count      = "2"
+        vm_size         = "Standard_D2_v2"
+    }
+
+    service_principal {
+        client_id     = ${var.client_id}
+        client_secret = ${var.client_secret}
+    }
+
+    network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin = "kubenet"
+    }
+
+    tags = {
+        Environment = "Development"
+    }
+}
