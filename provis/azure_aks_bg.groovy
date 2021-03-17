cc08cd1428885671bea75c9aa3fc3f7722083c8c
kjh1234 <kjh1234@gmail.com>
2021-03-17 17:21:54 +0900

    Update azure_aks_bg.groovy

--- provis/azure_aks_bg.groovy
+++ provis/azure_aks_bg.groovy
@@ -97,16 +97,16 @@ pipeline {
             dns_name="\$2"
             IP=
             while true; do
-                echo "Waiting external IP for $service..."
+                echo "Waiting external IP for \$service..."
                 IP="\$(kubectl get service "\$service" --kubeconfig "\$kubeconfig" | tail -n +2 | awk '{print \$4}' | grep -v '<')"
-                if [[ "$?" == 0 && -n "\$IP" ]]; then
+                if [[ "\$?" == 0 && -n "\$IP" ]]; then
                     echo "Service \$service public IP: \$IP"
                     break
                 fi
                 sleep 10
             done
 
-            public_ip="$(az network public-ip list -g "\$companion_rg" --query "[?ipAddress==\`\$IP\`] | [0].id" -o tsv)"
+            public_ip="\$(az network public-ip list -g "\$companion_rg" --query "[?ipAddress==\`\$IP\`] | [0].id" -o tsv)"
             if [[ -z "\$public_ip" ]]; then
                 echo "Cannot find public IP resource ID for '\$service' in companion resource group '\$companion_rg'" >&2
                 exit 1
@@ -114,7 +114,7 @@ pipeline {
 
             echo "Assign DNS name '\$dns_name' for '\$service'"
             az network public-ip update --dns-name "\$dns_name" --ids "\$public_ip"
-            [[ $? != 0 ]] && exit 1
+            [[ \$? != 0 ]] && exit 1
         }
 
         assign_dns todoapp-service "aks-todoapp-dns"
