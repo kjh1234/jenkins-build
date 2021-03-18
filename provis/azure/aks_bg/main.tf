190e260f4d5b2f31157e8135ad68c503ad9a9b89
kjh1234 <kjh1234@gmail.com>
2021-03-18 10:20:05 +0900

    Update main.tf

--- provis/azure/aks_bg/main.tf
+++ provis/azure/aks_bg/main.tf
@@ -16,14 +16,14 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     location            = "${azurerm_resource_group.main.location}"
     resource_group_name = "${azurerm_resource_group.main.name}"
     dns_prefix          = "dns"
+    kubernetes_version  = "1.18.14"
 
-    linux_profile {
-        admin_username = "ubuntu"
-
-        ssh_key {
-            key_data = "${file(var.public_key)}"
-        }
-    }
+    # linux_profile {
+    #     admin_username = "ubuntu"
+    #     ssh_key {
+    #         key_data = "${file(var.public_key)}"
+    #     }
+    # }
 
     default_node_pool {
         name            = "agentpool"
@@ -31,17 +31,17 @@ resource "azurerm_kubernetes_cluster" "k8s" {
         vm_size         = "Standard_D2_v2"
     }
 
-    service_principal {
-        client_id     = "${var.client_id}"
-        client_secret = "${var.client_secret}"
-    }
+    # service_principal {
+    #     client_id     = "${var.client_id}"
+    #     client_secret = "${var.client_secret}"
+    # }
 
-    network_profile {
-    load_balancer_sku = "Basic"
-    network_plugin = "kubenet"
-    }
+    # network_profile {
+    #   load_balancer_sku = "Basic"
+    #   network_plugin = "kubenet"
+    # }
 
-    tags = {
-        Environment = "Development"
-    }
+    # tags = {
+    #     Environment = "Development"
+    # }
 }
