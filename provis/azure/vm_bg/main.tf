9fc0501be3b8e0beb007fe854b08b5c66faab8ec
kjh1234 <kjh1234@gmail.com>
2021-03-19 15:08:45 +0900

    Create main.tf

---
+++ provis/azure/vm_bg/main.tf
@@ -0,0 +1,178 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
+}
+
+data "azurerm_resource_group" "image" {
+  name = "${var.image_resource_group_name}"
+}
+
+data "azurerm_image" "image" {
+  name                = "${var.image_name}"
+  resource_group_name = "${data.azurerm_resource_group.image.name}"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.app_resource_group_name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vmssbg-vnet"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = "${azurerm_resource_group.main.location}"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "vmssbg-subnet"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "main" {
+  name                         = "vmssbg-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.main.name}"
+  allocation_method            = "Static"
+  sku                          = "standard"
+}
+
+resource "azurerm_lb" "main" {
+  name                = "vmssbg-lb"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  sku                          = "standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.main.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.main.id}"
+  name                = "blue-bepool"
+}
+
+resource "azurerm_lb_nat_pool" "main" {
+  resource_group_name            = "${azurerm_resource_group.main.name}"
+  name                           = "blue-natpool"
+  loadbalancer_id                = "${azurerm_lb.main.id}"
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_backend_address_pool" "green" {
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.main.id}"
+  name                = "green-bepool"
+}
+
+resource "azurerm_lb_nat_pool" "green" {
+  resource_group_name            = "${azurerm_resource_group.main.name}"
+  name                           = "green-natpool"
+  loadbalancer_id                = "${azurerm_lb.main.id}"
+  protocol                       = "Tcp"
+  frontend_port_start            = 50120
+  frontend_port_end              = 50239
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_probe" "main" {
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.main.id}"
+  name                = "tomcat"
+  port                = "${var.application_port}"
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "vmssbg-nsg"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  security_rule {
+    name                       = "allow-public-access"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "22-55000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_virtual_machine_scale_set" "main" {
+  name                = "vmssbg-blue"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_D1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  os_profile {
+    computer_name_prefix = "vmss"
+    admin_username       = "${var.admin_id}"
+    admin_password       = "${var.admin_password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  network_profile {
+    name    = "web_ss_net_profile"
+    primary = true
+
+    network_security_group_id = "${azurerm_network_security_group.main.id}"
+    ip_configuration {
+      name      = "vmssbg-subnet"
+      subnet_id = "${azurerm_subnet.main.id}"
+      primary   = true
+      load_balancer_backend_address_pool_ids = ["${azurerm_lb_backend_address_pool.main.id}"]
+      load_balancer_inbound_nat_rules_ids    =  ["${azurerm_lb_nat_pool.main.id}"]
+    }
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_image_reference {
+    id="${data.azurerm_image.image.id}"
+  }
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  resource_group_name            = "${var.app_resource_group_name}"
+  loadbalancer_id                = "${azurerm_lb.main.id}"
+  name                           = "tomcat"
+  protocol                       = "Tcp"
+  frontend_port                  = "${var.frontend_port}"
+  backend_port                   = "${var.application_port}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.main.id}"
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = "${azurerm_lb_probe.main.id}"
+}
