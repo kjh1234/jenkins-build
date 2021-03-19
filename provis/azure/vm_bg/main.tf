fc7ad8da45ed63769ba4cb98bd72e39cafdeb139
kjh1234 <kjh1234@gmail.com>
2021-03-19 16:49:45 +0900

    Update main.tf

--- provis/azure/vm_bg/main.tf
+++ provis/azure/vm_bg/main.tf
@@ -49,6 +49,12 @@ resource "azurerm_virtual_machine" "main" {
     admin_username       = "${var.admin_id}"
     admin_password       = "${var.admin_password}"
   }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
 
   os_profile_linux_config {
     disable_password_authentication = false
