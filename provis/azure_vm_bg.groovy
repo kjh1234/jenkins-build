24a00ce8df422810a968db27f3901df56ca671cc
kjh1234 <kjh1234@gmail.com>
2021-03-22 15:56:51 +0900

    Update azure_vm_bg.groovy

--- provis/azure_vm_bg.groovy
+++ provis/azure_vm_bg.groovy
@@ -33,7 +33,7 @@ pipeline {
             cd ${workspace}/provis/azure/vm_bg
             terraform plan -out=tfplan -input=false \
               -var 'app_resource_group_name=${RESOURCE_GROUP}' \
-              -var "public_key=$(cat ${PUBLIC_KEY})" \
+              -var "public_key=\$(cat ${PUBLIC_KEY})" \
               -var 'client_id=${AZURE_CLIENT_ID}' \
               -var 'client_secret=${AZURE_CLIENT_SECRET}' \
               -var 'tenant_id=${AZURE_TENANT_ID}' \
