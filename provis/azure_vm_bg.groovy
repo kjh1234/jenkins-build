0349df164d0658cccc864b54594241bf769879c4
kjh1234 <kjh1234@gmail.com>
2021-03-22 15:04:17 +0900

    Update azure_vm_bg.groovy

--- provis/azure_vm_bg.groovy
+++ provis/azure_vm_bg.groovy
@@ -70,7 +70,7 @@ pipeline {
   environment {
     INNO_AZURE_CREDENTIALS = 'INNO_AZURE_CREDENTIALS'
     AZURE_SUBSCRIPTION_ID = credentials('AZURE_SUBSCRIPTION_ID')
-    PUBLIC_KEY="~/.ssh/vm-halyard-ubuntu-key.pem"
+    PUBLIC_KEY="~/.ssh/inno_id_rsa2.pub"
     RESOURCE_GROUP="vm-bg-tf-jenkins"
   }
 }
