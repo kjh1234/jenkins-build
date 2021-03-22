68b5ada5868543be4bc7c643ae08351058f9c5a8
kjh1234 <kjh1234@gmail.com>
2021-03-22 13:34:51 +0900

    Update main.tf

--- provis/azure/vm_bg/main.tf
+++ provis/azure/vm_bg/main.tf
@@ -79,16 +79,13 @@ resource "azurerm_virtual_machine" "main" {
     admin_username       = "${var.admin_id}"
     custom_data          = file("cloud-init.yml")
   }
-  os_profile {
-      admin_username = "azureuser"
-  }
 
   os_profile_linux_config {
       disable_password_authentication = true
 
-      ssh_keys = [{
+      ssh_keys = {
           key_data = "${file(var.public_key)}"
-      }]
+      }
   }
 
   storage_image_reference {
