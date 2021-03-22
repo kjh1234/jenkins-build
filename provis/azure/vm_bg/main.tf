e2834783df4b6ae9e113ee479d925b8dbb411375
kjh1234 <kjh1234@gmail.com>
2021-03-22 13:37:45 +0900

    Update main.tf

--- provis/azure/vm_bg/main.tf
+++ provis/azure/vm_bg/main.tf
@@ -83,7 +83,7 @@ resource "azurerm_virtual_machine" "main" {
   os_profile_linux_config {
       disable_password_authentication = true
 
-      ssh_key = {
+      ssh_keys {
           key_data = "${file(var.public_key)}"
       }
   }
