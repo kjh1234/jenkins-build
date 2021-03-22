a536c9a20721fa3a3ef0035e819f6213b8a0862a
kjh1234 <kjh1234@gmail.com>
2021-03-22 13:48:12 +0900

    Update main.tf

--- provis/azure/vm_bg/main.tf
+++ provis/azure/vm_bg/main.tf
@@ -102,8 +102,8 @@ resource "azurerm_virtual_machine" "main" {
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+}
 
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+output "public_ip_address" {
+  value = "${data.azurerm_public_ip.main.*.ip_address}"
 }
