af9f0ca2dc3cdb6813c550d8a911b85bdd6bc30d
kjh1234 <kjh1234@gmail.com>
2021-03-23 13:30:41 +0900

    Update main.tf

--- provis/azure/function/main.tf
+++ provis/azure/function/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "main" {
 }
 
 resource "azurerm_storage_account" "main" {
-  name                     = "funcStorageAccount"
+  name                     = "funcstgaccount"
   resource_group_name      = "${azurerm_resource_group.main.name}"
   location                 = "${azurerm_resource_group.main.location}"
   account_tier             = "Standard"
