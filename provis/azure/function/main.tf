0f1ec37a272b361e512e289b7fbcf4abc89c817d
kjh1234 <kjh1234@gmail.com>
2021-03-23 14:06:40 +0900

    Update main.tf

--- provis/azure/function/main.tf
+++ provis/azure/function/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_app_service_plan" "main" {
 }
 
 resource "azurerm_function_app" "main" {
-  name                       = "az-func"
+  name                       = "inno-func-app"
   location                   = "${azurerm_resource_group.main.location}"
   resource_group_name        = "${azurerm_resource_group.main.name}"
   app_service_plan_id        = "${azurerm_app_service_plan.main.id}"
