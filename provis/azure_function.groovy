85dfc7f4051412ad4d28c078f5282afe52d4d08e
kjh1234 <kjh1234@gmail.com>
2021-03-23 14:11:34 +0900

    Update azure_function.groovy

--- provis/azure_function.groovy
+++ provis/azure_function.groovy
@@ -68,7 +68,7 @@ pipeline {
   environment {
     INNO_AZURE_CREDENTIALS = 'INNO_AZURE_CREDENTIALS'
     AZURE_SUBSCRIPTION_ID = credentials('AZURE_SUBSCRIPTION_ID')
-    RESOURCE_GROUP="func-tf-jenkins"
+    RESOURCE_GROUP="func-tf-jenkins-1"
     LOCATIONS="koreacentral"
     
   }
