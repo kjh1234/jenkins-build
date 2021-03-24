e7b5464d90d7cb549944721c2034697ec703b573
kjh1234 <kjh1234@gmail.com>
2021-03-24 16:42:52 +0900

    Update main.tf

--- provis/azure/vm_image_bg/main.tf
+++ provis/azure/vm_image_bg/main.tf
@@ -123,7 +123,6 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.main.id}"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.main.id}"
   }
 }
 
