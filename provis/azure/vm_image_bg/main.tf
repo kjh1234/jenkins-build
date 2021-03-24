56f6c0062177303180e290b2a1711ff476ee856e
kjh1234 <kjh1234@gmail.com>
2021-03-24 16:36:37 +0900

    Update main.tf

--- provis/azure/vm_image_bg/main.tf
+++ provis/azure/vm_image_bg/main.tf
@@ -36,7 +36,8 @@ resource "azurerm_public_ip" "main" {
   name                 = "vm-pip"
   location             = "${azurerm_resource_group.main.location}"
   resource_group_name  = "${azurerm_resource_group.main.name}"
-  allocation_method    = "Dynamic"
+  allocation_method            = "Static"
+  sku                          = "standard"
 }
 
 resource "azurerm_lb" "main" {
