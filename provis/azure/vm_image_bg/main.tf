13dc8a219de07162dd659fc186f69a72392f6f53
kjh1234 <kjh1234@gmail.com>
2021-03-25 10:23:32 +0900

    Update main.tf

--- provis/azure/vm_image_bg/main.tf
+++ provis/azure/vm_image_bg/main.tf
@@ -58,16 +58,16 @@ resource "azurerm_lb_backend_address_pool" "main" {
   name                = "blue-bepool"
 }
 
-resource "azurerm_lb_nat_pool" "main" {
-  resource_group_name            = "${azurerm_resource_group.main.name}"
-  name                           = "blue-natpool"
-  loadbalancer_id                = "${azurerm_lb.main.id}"
-  protocol                       = "Tcp"
-  frontend_port_start            = 50000
-  frontend_port_end              = 50119
-  backend_port                   = 22
-  frontend_ip_configuration_name = "PublicIPAddress"
-}
+# resource "azurerm_lb_nat_pool" "main" {
+#   resource_group_name            = "${azurerm_resource_group.main.name}"
+#   name                           = "blue-natpool"
+#   loadbalancer_id                = "${azurerm_lb.main.id}"
+#   protocol                       = "Tcp"
+#   frontend_port_start            = 50000
+#   frontend_port_end              = 50119
+#   backend_port                   = 22
+#   frontend_ip_configuration_name = "PublicIPAddress"
+# }
 # resource "azurerm_lb_nat_rule" "main" {
 #   resource_group_name            = "${azurerm_resource_group.main.name}"
 #   loadbalancer_id                = "${azurerm_lb.main.id}"
@@ -78,22 +78,22 @@ resource "azurerm_lb_nat_pool" "main" {
 #   frontend_ip_configuration_name = "PublicIPAddress"
 # }
 
-resource "azurerm_lb_backend_address_pool" "green" {
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  loadbalancer_id     = "${azurerm_lb.main.id}"
-  name                = "green-bepool"
-}
+# resource "azurerm_lb_backend_address_pool" "green" {
+#   resource_group_name = "${azurerm_resource_group.main.name}"
+#   loadbalancer_id     = "${azurerm_lb.main.id}"
+#   name                = "green-bepool"
+# }
 
-resource "azurerm_lb_nat_pool" "green" {
-  resource_group_name            = "${azurerm_resource_group.main.name}"
-  name                           = "green-natpool"
-  loadbalancer_id                = "${azurerm_lb.main.id}"
-  protocol                       = "Tcp"
-  frontend_port_start            = 50120
-  frontend_port_end              = 50239
-  backend_port                   = 22
-  frontend_ip_configuration_name = "PublicIPAddress"
-}
+# resource "azurerm_lb_nat_pool" "green" {
+#   resource_group_name            = "${azurerm_resource_group.main.name}"
+#   name                           = "green-natpool"
+#   loadbalancer_id                = "${azurerm_lb.main.id}"
+#   protocol                       = "Tcp"
+#   frontend_port_start            = 50120
+#   frontend_port_end              = 50239
+#   backend_port                   = 22
+#   frontend_ip_configuration_name = "PublicIPAddress"
+# }
 # resource "azurerm_lb_nat_rule" "green" {
 #   resource_group_name            = "${azurerm_resource_group.main.name}"
 #   loadbalancer_id                = "${azurerm_lb.main.id}"
@@ -141,16 +141,16 @@ resource "azurerm_network_interface" "main" {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.main.id}"
     private_ip_address_allocation = "Dynamic"
-    load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.main.id}"]
-    load_balancer_inbound_nat_rules_ids     = ["${azurerm_lb_nat_pool.main.id}"]
+#     load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.main.id}"]
+#     load_balancer_inbound_nat_rules_ids     = ["${azurerm_lb_nat_pool.main.id}"]
   }
 }
 
-# resource "azurerm_network_interface_backend_address_pool_association" "main" {
-#   network_interface_id    = "${azurerm_network_interface.main.id}"
-#   ip_configuration_name   = "testconfiguration1"
-#   backend_address_pool_id = "${azurerm_lb_backend_address_pool.main.id}"
-# }
+resource "azurerm_network_interface_backend_address_pool_association" "main" {
+  network_interface_id    = "${azurerm_network_interface.main.id}"
+  ip_configuration_name   = "testconfiguration1"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.main.id}"
+}
 
 # resource "azurerm_network_interface_nat_rule_association" "main" {
 #   network_interface_id  = "${azurerm_network_interface.main.id}"
