051c4ce76ff57cf9262d19a381ec7335b7eb666b
kjh1234 <kjh1234@gmail.com>
2021-03-26 11:56:37 +0900

    Update lb_rule.tf

--- provis/azure/vm_dup_bg/lb_rule.tf
+++ provis/azure/vm_dup_bg/lb_rule.tf
@@ -1,9 +1,15 @@
-
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
+}
 
 resource "azurerm_lb_rule" "lbnatrule" {
   resource_group_name            = "${var.app_resource_group_name}"
   loadbalancer_id                = "${azurerm_lb.main.id}"
-  name                           = "tomcat"
+  name                           = "${pool_name}-rule"
   protocol                       = "Tcp"
   frontend_port                  = "${var.frontend_port}"
   backend_port                   = "${var.application_port}"
