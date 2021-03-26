2a8f7618db28fa4b5d662adcdbf688beb848e2e0
kjh1234 <kjh1234@gmail.com>
2021-03-26 16:00:35 +0900

    Update main.tf

--- provis/azure/modules/lb_pool_nic/main.tf
+++ provis/azure/modules/lb_pool_nic/main.tf
@@ -7,11 +7,11 @@ resource "azurerm_lb_backend_address_pool" "main" {
 }
 
 resource "azurerm_lb_probe" "main" {
-  count = length(var.system_types)
+  count = length(var.pool_names)
   
   resource_group_name = "${var.app_resource_group_name}"
   loadbalancer_id     = "${var.lb_id}"
-  name                = "${element(var.system_types, count.index)}-tomcat"
+  name                = "${element(var.pool_names, count.index)}-tomcat"
   port                = "${var.application_port}"
 }
 
