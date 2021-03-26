e22e116ff8edbd9ae7edb53faa7f3d0c95cc59f6
kjh1234 <kjh1234@gmail.com>
2021-03-26 11:58:35 +0900

    Update lb_pool.tf

--- provis/azure/vm_dup_bg/lb_pool.tf
+++ provis/azure/vm_dup_bg/lb_pool.tf
@@ -34,7 +34,7 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
   network_interface_id    = "${azurerm_network_interface.main.id}"
-  ip_configuration_name   = "${pool_name}configuration1"
+  ip_configuration_name   = "${pool_name}-configuration"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.main.id}"
 }
 
