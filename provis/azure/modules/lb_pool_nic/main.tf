3869a3e14404655d6f935718e89f47b9e02e225c
kjh1234 <kjh1234@gmail.com>
2021-03-26 14:32:37 +0900

    Update main.tf

--- provis/azure/modules/lb_pool_nic/main.tf
+++ provis/azure/modules/lb_pool_nic/main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "${var.subscription_id}"
-  client_id = "${var.client_id}"
-  client_secret = "${var.client_secret}"
-  tenant_id = "${var.tenant_id}"
-}
-
 resource "azurerm_lb_backend_address_pool" "main" {
   resource_group_name = "${var.app_resource_group_name}"
   loadbalancer_id     = "${azurerm_lb.main.id}"
