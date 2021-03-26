d5accf3e1825df1148abfefae1580e5e4afa43ff
kjh1234 <kjh1234@gmail.com>
2021-03-26 14:59:57 +0900

    Update output.tf

--- provis/azure/modules/lb_pool_nic/output.tf
+++ provis/azure/modules/lb_pool_nic/output.tf
@@ -1,7 +1,7 @@
 output "lb_backend_address_pool_id" {
-  value = azurerm_lb_backend_address_pool.main.id
+  value = azurerm_lb_backend_address_pool.main.*.id
 }
 
 output "network_interface_id" {
-  value = azurerm_network_interface.main.id
+  value = azurerm_network_interface.main.*.id
 }
