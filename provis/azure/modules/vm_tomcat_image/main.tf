a4774f787c8ddb4eb1f5aed24cf8a0e3fb83a4dc
kjh1234 <kjh1234@gmail.com>
2021-03-26 15:54:43 +0900

    Update main.tf

--- provis/azure/modules/vm_tomcat_image/main.tf
+++ provis/azure/modules/vm_tomcat_image/main.tf
@@ -40,3 +40,14 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
+resource "azurerm_lb_rule" "lbnatrule" {
+  resource_group_name            = "${var.app_resource_group_name}"
+  loadbalancer_id                = "${var.lb_id}"
+  name                           = "${var.system_type}-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = "${var.frontend_port}"
+  backend_port                   = "${var.application_port}"
+  backend_address_pool_id        = "${var.lb_backend_address_pool_id}"
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = "${var.lb_probe_id}"
+}
