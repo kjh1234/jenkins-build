1a520d799e0bc8d346a102c7ba3d82eb804f11c7
kjh1234 <kjh1234@gmail.com>
2021-03-26 15:27:43 +0900

    Update variables.tf

--- provis/azure/modules/vm_tomcat_image/variables.tf
+++ provis/azure/modules/vm_tomcat_image/variables.tf
@@ -1,20 +1,3 @@
-#     Credential variables     #
-variable "subscription_id" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
-variable "client_id" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
-variable "client_secret" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
-variable "tenant_id" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
 #     resource variables     #
 variable "app_resource_group_name" {
   description = "Resource Group name, must be lowercase alphanumeric with hyphens as its used as domain_name_label as well"
