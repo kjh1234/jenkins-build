e7797b542bfb76a4f1524310962f6fe7b2f081f4
kjh1234 <kjh1234@gmail.com>
2021-03-26 14:40:32 +0900

    Update main.tf

--- provis/azure/modules/lb_pool_nic/main.tf
+++ provis/azure/modules/lb_pool_nic/main.tf
@@ -1,36 +1,36 @@
 resource "azurerm_lb_backend_address_pool" "main" {
   resource_group_name = "${var.app_resource_group_name}"
   loadbalancer_id     = "${azurerm_lb.main.id}"
-  name                = "${pool_name}-bepool"
+  name                = "${var.pool_name}-bepool"
 }
 
 resource "azurerm_lb_probe" "main" {
   resource_group_name = "${var.app_resource_group_name}"
   loadbalancer_id     = "${azurerm_lb.main.id}"
-  name                = "${pool_name}-tomcat"
+  name                = "${var.pool_name}-tomcat"
   port                = "${var.application_port}"
 }
 
 # Create network interface
 resource "azurerm_network_interface" "main" {
-  name                = "${prefix}-nic-${pool_name}"
+  name                = "${prefix}-nic-${var.pool_name}"
   location            = "${var.location}"
   resource_group_name = "${var.app_resource_group_name}"
 
   ip_configuration {
-    name                          = "${pool_name}-configuration"
-    subnet_id                     = "${azurerm_subnet.main.id}"
+    name                          = "${var.pool_name}-configuration"
+    subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
   network_interface_id    = "${azurerm_network_interface.main.id}"
-  ip_configuration_name   = "${pool_name}-configuration"
+  ip_configuration_name   = "${var.pool_name}-configuration"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.main.id}"
 }
 
 resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id          = "${azurerm_network_interface.main.id}"
-  network_security_group_id     = "${azurerm_network_security_group.main.id}"
+  network_security_group_id     = "${var.nsg_id}"
 }
