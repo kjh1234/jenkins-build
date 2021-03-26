b54752b105d9ac92b939a2cd7d75fd190e4b94dd
kjh1234 <kjh1234@gmail.com>
2021-03-26 11:46:58 +0900

    Update vm.tf

--- provis/azure/vm_dup_bg/vm.tf
+++ provis/azure/vm_dup_bg/vm.tf
@@ -1,4 +1,10 @@
-
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
+}
 
 # Locate the existing custom/golden image
 data "azurerm_image" "main" {
@@ -7,31 +13,27 @@ data "azurerm_image" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
-  name                  = "vm-${pool_name}-COUNT"
+  count                 = ${vm_instances}
+  
+  name                  = "vm-${pool_name}-${count.index}"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
   vm_size               = "Standard_DS1_v2"
   network_interface_ids = [azurerm_network_interface.main.id]
 
-#   storage_image_reference {
-#     publisher = "Canonical"
-#     offer     = "UbuntuServer"
-#     sku       = "16.04-LTS"
-#     version   = "latest"
-#   }
   storage_image_reference {
     id = "${data.azurerm_image.main.id}"
   }
 
   storage_os_disk {
-    name              = "myosdisk-${pool_name}-COUNT"
+    name              = "myosdisk-${pool_name}-${count.index}"
     caching       = "ReadWrite"
     managed_disk_type = "Standard_LRS"
     create_option = "FromImage"
   }
 
   os_profile {
-    computer_name  = "todo-vm-${pool_name}-COUNT"
+    computer_name  = "todo-vm-${pool_name}-${count.index}"
     admin_username = "${var.admin_id}"
     admin_password = "${var.admin_password}"
   }
