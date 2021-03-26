c9bcdb978a03d7d2d7823d52a3062434e543eac1
kjh1234 <kjh1234@gmail.com>
2021-03-26 14:26:55 +0900

    Update variables.tf

--- provis/azure/modules/lb_pool_nic/variables.tf
+++ provis/azure/modules/lb_pool_nic/variables.tf
@@ -1,20 +1,3 @@
-#     Credential variables     #
-variable "subscription_id" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
-variable "client_id" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
-variable "client_secret" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
-variable "tenant_id" {
-  description = "Should be on the credentials file if not you must generate it."
-}
-
 #     resource variables     #
 variable "app_resource_group_name" {
   description = "Resource Group name, must be lowercase alphanumeric with hyphens as its used as domain_name_label as well"
@@ -24,6 +7,14 @@ variable "app_resource_group_name" {
 variable "location" {
   description = "The location where resources are created"
   default     = "koreacentral"
+}
+
+variable "nsg_id" {
+  default     = ""
+}
+
+variable "nic_id" {
+  default     = ""
 }
 
 variable "prefix" {
