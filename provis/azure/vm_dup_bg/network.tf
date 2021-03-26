bf0309499ca0d819b509a306f3efd2a7ac62c789
kjh1234 <kjh1234@gmail.com>
2021-03-26 11:26:23 +0900

    Create network.tf

---
+++ provis/azure/vm_dup_bg/network.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vm-vnet"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  location            = "${azurerm_resource_group.main.location}"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "vm-subnet"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "main" {
+  name                 = "vm-pip"
+  location             = "${azurerm_resource_group.main.location}"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  allocation_method            = "Static"
+  sku                          = "standard"
+}
