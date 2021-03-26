ba65c3e36f1c1a5223c27a6d83a9d45cf29611ed
kjh1234 <kjh1234@gmail.com>
2021-03-26 14:35:43 +0900

    Update output.tf

--- provis/azure/modules/lb_network/output.tf
+++ provis/azure/modules/lb_network/output.tf
@@ -2,6 +2,10 @@ output "public_ip_address" {
   value = azurerm_public_ip.main.*.ip_address
 }
 
+output "subnet_id" {
+  value = azurerm_subnet.main.id
+}
+
 output "nsg_id" {
   value = azurerm_network_security_group.main.id
 }
