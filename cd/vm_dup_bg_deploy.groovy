3258f81cf44c46cf4e89864413dea59ff7f62ed3
kjh1234 <kjh1234@gmail.com>
2021-03-29 15:47:17 +0900

    Update vm_dup_bg_deploy.groovy

--- cd/vm_dup_bg_deploy.groovy
+++ cd/vm_dup_bg_deploy.groovy
@@ -133,8 +133,8 @@ pipeline {
 		// oldDisks = sh(returnStdout: true, script: "az disk list -g $RESOURCE_GROUP --query \"[?contains(name, '$currentBackend')].id\" -o tsv").trim()
 		// oldNICs = sh(returnStdout: true, script: "az network nic list -g $RESOURCE_GROUP  --query \"[?contains(name, '$currentBackend')].id\" -o tsv").trim()
 		sh """
-		  az vm delete --yes --ids \$(az vm list -g $RESOURCE_GROUP --query "[?contains(name, '$currentBackend')].id" -o tsv)
-		  az disk delete --yes --ids \$(az disk list -g $RESOURCE_GROUP --query "[?contains(name, '$currentBackend')].id" -o tsv)
+		  # az vm delete --yes --ids \$(az vm list -g $RESOURCE_GROUP --query "[?contains(name, '$currentBackend')].id" -o tsv)
+		  # az disk delete --yes --ids \$(az disk list -g $RESOURCE_GROUP --query "[?contains(name, '$currentBackend')].id" -o tsv)
 		  az network nic delete --ids \$(az network nic list -g $RESOURCE_GROUP  --query "[?contains(name, '$currentBackend')].id" -o tsv)
 		"""
 	 }
