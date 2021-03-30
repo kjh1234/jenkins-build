2b99c3003d0e76b1582405c32cc94f5cd77991cd
kjh1234 <kjh1234@gmail.com>
2021-03-30 16:08:36 +0900

    Create variables.tf

---
+++ provis/azure/modules/public-vm/variables.tf
@@ -0,0 +1,44 @@
+#     resource variables     #
+variable "app_resource_group_name" {
+  description = "Resource Group name, must be lowercase alphanumeric with hyphens as its used as domain_name_label as well"
+  default     = "vm-tf-jenkins"
+}
+
+variable "location" {
+  description = "The location where resources are created"
+  default     = "koreacentral"
+}
+
+variable "nsg_id" {
+  default     = ""
+}
+
+variable "subnet_id" {
+  default     = ""
+}
+
+variable "prefix" {
+  default     = "default"
+}
+
+variable "pool_name" {
+  default     = "default"
+}
+
+variable "admin_id" {
+    description = "Default password for azureuser"
+    default = "azureuser"
+}
+
+variable "admin_password" {
+    description = "Default password for azureuser"
+    default = "dlatl!1234"
+}
+
+variable "public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "image_version" {
+  default     = "7"
+}
