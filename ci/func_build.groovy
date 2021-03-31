b305653c455d8908c11eeb43ba76402bc7c75d31
권준호 SKC&C/SK텔레콤_유관부서 <yamanin1@partner.sk.com>
2021-03-31 17:00:37 +0900

    ZIP 이미지 Nexus Push

--- ci/func_build.groovy
+++ ci/func_build.groovy
@@ -15,20 +15,22 @@ pipeline {
             $class: 'GitSCM',
             branches: [[name: "refs/tags/${params.TAG_VERSION}"]],
             doGenerateSubmoduleConfigurations: false,
-            extensions: [[
-              $class: 'SubmoduleOption',
-              disableSubmodules: false,
-              parentCredentials: false,
-              recursiveSubmodules: false,
-              reference: '',
-              trackingSubmodules: false
-            ]],
+            extensions: [
+              [
+                $class: 'SubmoduleOption',
+                disableSubmodules: false,
+                parentCredentials: false,
+                recursiveSubmodules: false,
+                reference: '',
+                trackingSubmodules: false
+              ]
+            ],
             submoduleCfg: [],
-            userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/azure-functions-samples-java.git"]]
+            userRemoteConfigs: [
+              [credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/azure-functions-samples-java.git"]]
             // userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/hello-spring-function-azure.git"]]
           ])
         }
-
       }
     }
 
@@ -42,40 +44,55 @@ pipeline {
             chmod 764 ./mvnw
             ./mvnw clean package
 
-            cd ${workspace}/target/azure-functions/inno-func-app && zip -r ../../../archive.zip ./* && cd -
+            cd ${workspace}/target/azure-functions/inno-func-app && zip -r ../../../azure-functions-samples-${params.TAG_VERSION}.zip ./* && cd -
           """
         }
-
       }
     }
 
-    stage('function stage Deploy') {
+    stage('Image Push') {
       steps {
-        sh """
-          az functionapp deployment slot create -g ${RESOURCE_GROUP} -n ${FUNC_NAME} --slot stage
-          az functionapp deployment source config-zip -g ${RESOURCE_GROUP} -n ${FUNC_NAME} --slot stage --src ./archive.zip
-        """
+        withCredentials(usernamePassword(credentialsId: NEXUS_CREDENTIALS_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')) {
+          sh """
+            curl -v -u '${USERNAME}:${PASSWORD}' POST 'https://doss.sktelecom.com/nexus/service/rest/v1/components?repository=sk-maven-hosted'
+              -F maven2.groupId=com.functions
+              -F maven2.artifactId=azure-functions-samples
+              -F maven2.version=${params.TAG_VERSION}
+              -F maven2.asset1=${workspace}/target/azure-functions/inno-func-app/azure-functions-samples-${params.TAG_VERSION}.zip
+              -F maven2.asset1.extension=zip
+              -F maven2.generate-pom=true
+          """
+        }
       }
     }
 
-    stage('Switch') {
-      steps {
-        input("Switch Prod Proceed or Abort?")
-				  
-        sh "az functionapp deployment slot swap -g func-tf-jenkins -n inno-tf-func-app --slot stage --target-slot production"
-      }
-    }
-
-    stage('Delete Stage') {
-      steps {
-	script {
-	  result = input(message: 'Delete Stage-Slot?', ok: 'Proceed', parameters: [booleanParam(defaultValue: true, name: 'Yes?')])
-          if (result == true) {
-	    sh "az functionapp deployment slot delete -g ${RESOURCE_GROUP} -n ${FUNC_NAME} --slot stage"
-          }
-	}
-      }
-    }
+//    stage('function stage Deploy') {
+//      steps {
+//        sh """
+//          az functionapp deployment slot create -g ${RESOURCE_GROUP} -n ${FUNC_NAME} --slot stage
+//          az functionapp deployment source config-zip -g ${RESOURCE_GROUP} -n ${FUNC_NAME} --slot stage --src ./archive.zip
+//        """
+//      }
+//    }
+//
+//    stage('Switch') {
+//      steps {
+//        input("Switch Prod Proceed or Abort?")
+//
+//        sh "az functionapp deployment slot swap -g func-tf-jenkins -n inno-tf-func-app --slot stage --target-slot production"
+//      }
+//    }
+//
+//    stage('Delete Stage') {
+//      steps {
+//        script {
+//          result = input(message: 'Delete Stage-Slot?', ok: 'Proceed', parameters: [booleanParam(defaultValue: true, name: 'Yes?')])
+//          if (result == true) {
+//            sh "az functionapp deployment slot delete -g ${RESOURCE_GROUP} -n ${FUNC_NAME} --slot stage"
+//          }
+//        }
+//      }
+//    }
 
     stage('Destroy') {
       steps {
@@ -84,15 +101,14 @@ pipeline {
           az logout
           """
         }
-
       }
     }
-
   }
   environment {
     AZURE_SUBSCRIPTION_ID = credentials('AZURE_SUBSCRIPTION_ID')
     INNO_AZURE_CREDENTIALS = 'INNO_AZURE_CREDENTIALS'
     GIT_CREDENTIALS_ID = credentials('GIT_CREDENTIALS_ID')
+    NEXUS_CREDENTIALS_ID = 'NEXUS_CREDENTIALS_ID'
     RESOURCE_GROUP = 'func-tf-jenkins'
     FUNC_NAME = 'inno-tf-func-app'
   }
