b914f079089b05cd9919d373be2d60fd4c0a80a3
kjh1234 <kjh1234@gmail.com>
2021-04-01 16:44:12 +0900

    Update func_deploy.groovy

--- cd/func_deploy.groovy
+++ cd/func_deploy.groovy
@@ -60,6 +60,8 @@ pipeline {
         withCredentials([usernamePassword(credentialsId: NEXUS_CREDENTIALS_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
           sh """
 	    curl -L -u '${USERNAME}:${PASSWORD}' -X GET '${REPOSITORY_API}/search/assets/download?repository=${IMAGE_REPOSITORY}&group=${IMAGE_GROUP}&name=${IMAGE_NAME}&version=${params.TAG_VERSION}&maven.extension=zip'
+	    
+	    ls -al
 	  """
         }
       }
