c63bc3fad42112aa7c47b4c9e85afd118227dfaf
kjh1234 <kjh1234@gmail.com>
2021-04-01 16:40:51 +0900

    Update func_deploy.groovy

--- cd/func_deploy.groovy
+++ cd/func_deploy.groovy
@@ -2,54 +2,69 @@ pipeline {
   agent any
   stages {
 
+//    stage('SCM') {
+//      steps {
+//        echo ' The SCM'
+//        script {          
+//          if (params.TAG_VERSION == '') {
+//            error "TAG_VERSION is required"
+//          }
+//          
+//          echo params.TAG_VERSION
+//          
+//          checkout([
+//              $class: 'GitSCM', 
+//              branches: [[name: "refs/tags/${params.TAG_VERSION}"]],
+//              doGenerateSubmoduleConfigurations: false, 
+//              extensions: [[
+//                  $class: 'SubmoduleOption', 
+//                  disableSubmodules: false, 
+//                  parentCredentials: false, 
+//                  recursiveSubmodules: false, 
+//                  reference: '', 
+//                  trackingSubmodules: false
+//              ]], 
+//              submoduleCfg: [], 
+//              userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/azure-functions-samples-java.git"]]
+//          ])
+//        }
+//
+//      }
+//    }
+//
+//    stage('Function Build/Deploy') {
+//      steps {
+//        // check the current active environment to determine the inactive one that will be deployed to
+//
+//        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
+//            // fetch the current service configuration
+//            sh """
+//              az login --service-principal -u "\$AZURE_CLIENT_ID" -p "\$AZURE_CLIENT_SECRET" -t "\$AZURE_TENANT_ID"
+//              az account set --subscription "\$AZURE_SUBSCRIPTION_ID"
+//              
+//	      chmod 764 ./mvnw
+//              ./mvnw clean package azure-functions:deploy
+//            """
+//        }
+//      }
+//    }
+
     stage('SCM') {
       steps {
-        echo ' The SCM'
-        script {          
-          if (params.TAG_VERSION == '') {
-            error "TAG_VERSION is required"
-          }
-          
-          echo params.TAG_VERSION
-          
-          checkout([
-              $class: 'GitSCM', 
-              branches: [[name: "refs/tags/${params.TAG_VERSION}"]],
-              doGenerateSubmoduleConfigurations: false, 
-              extensions: [[
-                  $class: 'SubmoduleOption', 
-                  disableSubmodules: false, 
-                  parentCredentials: false, 
-                  recursiveSubmodules: false, 
-                  reference: '', 
-                  trackingSubmodules: false
-              ]], 
-              submoduleCfg: [], 
-              userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/azure-functions-samples-java.git"]]
-          ])
-        }
-
+        checkout scm
       }
     }
-
-    stage('Function Build/Deploy') {
+	  
+    stage('Image Download') {
       steps {
-        // check the current active environment to determine the inactive one that will be deployed to
-
-        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
-            // fetch the current service configuration
-            sh """
-              az login --service-principal -u "\$AZURE_CLIENT_ID" -p "\$AZURE_CLIENT_SECRET" -t "\$AZURE_TENANT_ID"
-              az account set --subscription "\$AZURE_SUBSCRIPTION_ID"
-              
-	      chmod 764 ./mvnw
-              ./mvnw clean package azure-functions:deploy
-            """
+        withCredentials([usernamePassword(credentialsId: NEXUS_CREDENTIALS_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+          sh """
+	    curl -L -u '${USERNAME}:${PASSWORD}' -X GET '${REPOSITORY_API}/search/assets/download?repository=${IMAGE_REPOSITORY}&group=${IMAGE_GROUP}&name=${IMAGE_NAME}&version=${params.TAG_VERSION}&maven.extension=zip'
+	  """
         }
       }
     }
-
-
+	  
     stage('Destroy') {
       steps {
 	    script {
@@ -65,6 +80,10 @@ pipeline {
     AZURE_SUBSCRIPTION_ID = credentials('AZURE_SUBSCRIPTION_ID')
     INNO_AZURE_CREDENTIALS = "INNO_AZURE_CREDENTIALS"
     GIT_CREDENTIALS_ID = credentials('GIT_CREDENTIALS_ID')
+    REPOSITORY_API = "https://doss.sktelecom.com/nexus/service/rest/v1"
+    IMAGE_REPOSITORY = "sk-maven-hosted"
+    IMAGE_GROUP = "com.functions"
+    IMAGE_NAME = "azure-functions-samples"
   }
   parameters {
     string(name: 'TAG_VERSION', defaultValue: '', description: '')
