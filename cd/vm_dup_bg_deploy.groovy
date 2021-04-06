4cd2c3291982e610709d74842262f60ed3a6cf20
kjh1234 <kjh1234@gmail.com>
2021-04-06 16:21:54 +0900

    Update vm_dup_bg_deploy.groovy

--- cd/vm_dup_bg_deploy.groovy
+++ cd/vm_dup_bg_deploy.groovy
@@ -52,7 +52,7 @@ pipeline {
               }
 	      
 	      sh """
-	        scp -i VM_PRIBATE_KEY ${IMAGE_NAME}-${params.TAG_VERSION}.zip azureuser@${deployIp}:~/
+	        scp -i ${VM_PRIBATE_KEY} ${IMAGE_NAME}-${params.TAG_VERSION}.zip azureuser@${deployIp}:~/
 	      """
 	    }
 
