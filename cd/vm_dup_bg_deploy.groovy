8afbd695d8c47f007b76e17e26f17267bf53839c
kjh1234 <kjh1234@gmail.com>
2021-04-07 15:43:36 +0900

    Update vm_dup_bg_deploy.groovy

--- cd/vm_dup_bg_deploy.groovy
+++ cd/vm_dup_bg_deploy.groovy
@@ -36,137 +36,145 @@ pipeline {
 
       }
     }
-	  
-    stage('Terraform init'){
+    stage('cat test'){
       steps {
-        // Initialize the plan
-        sh  """
-         cd ${workspace}/${TERRAFORM_PATH}
-         terraform init -input=false
-        """
-      }
-    }
-
-    stage('Terraform plan'){
-      steps {
-
-        // Get the VM image ID for the VMSS
-        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
-          sh """
-            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
-            az account set --subscription $AZURE_SUBSCRIPTION_ID
-           
-            export ARM_CLIENT_ID="${AZURE_CLIENT_ID}"
-            export ARM_CLIENT_SECRET="${AZURE_CLIENT_SECRET}"
-            export ARM_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID}"
-            export ARM_TENANT_ID="${AZURE_TENANT_ID}"
-            
-            cd ${workspace}/${TERRAFORM_PATH}
-            terraform plan -out=tfplan -input=false \
-              -var 'app_resource_group_name=${RESOURCE_GROUP}' \
-              -var 'location=${LOCATION}' \
-              -var 'prefix=${PREFIX}' \
-              -var "pool_name=${newBackend()}" \
-              -var 'vm_instances=2' \
-              -var "public_key=\$(cat ${publicKey})" \
-              -var 'client_id=${AZURE_CLIENT_ID}' \
-              -var 'client_secret=${AZURE_CLIENT_SECRET}' \
-              -var 'tenant_id=${AZURE_TENANT_ID}' \
-              -var 'subscription_id=${AZURE_SUBSCRIPTION_ID}'
-            
-          """
-        }
-        
-      }
-    }
-
-    stage('Terraform apply'){
-      steps {
-        // Apply the plan
-        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
-          sh  """
-           cd ${workspace}/${TERRAFORM_PATH}
-           terraform apply -input=false -auto-approve "tfplan"
-          """
-        }
-      }
-    }
-
-    stage('APP Image Pull') {
-      steps {
-        script {
-          withCredentials([usernamePassword(credentialsId: NEXUS_CREDENTIALS_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-            sh """
-              curl -o ${IMAGE_NAME}-${params.TAG_VERSION}.jar -L -u '${USERNAME}:${PASSWORD}' \\
-                -X GET '${REPOSITORY_API}/search/assets/download?repository=${IMAGE_REPOSITORY}&group=${IMAGE_GROUP}&name=${IMAGE_NAME}&version=${params.TAG_VERSION}&maven.extension=jar'
-              
-              ls -al
-            """
-          }
-        }
-      }
-    }
-
-    stage('APP Deploy') {
-      steps {
-        script {
-          deployIp = sh(returnStdout: true, script: "az network public-ip show -g ${RESOURCE_GROUP} --name vm-dev-pip --query ipAddress --output tsv").trim()
-          privateIps = sh(returnStdout: true, script: "az network nic list -g ${RESOURCE_GROUP}  --query \"[?contains(name, '${newBackend()}')].ipConfigurations[].privateIpAddress\" -o tsv").split("\n")
-
-          print "deployIp : ${deployIp}"
-          print "privateIps : ${privateIps}"
-		
-          withCredentials([sshUserPrivateKey(credentialsId: VM_PRIBATE_KEY, keyFileVariable: 'identity', usernameVariable: 'userName')]) {  
-            for (privateIp in privateIps) {
-              sh """
-                # app push
-                scp -i '${identity}' -r -o ProxyCommand="ssh -i '${identity}' -W %h:%p azureuser@${deployIp}" ${IMAGE_NAME}-${params.TAG_VERSION}.zip azureuser@privateIp:~/
-                # app run
-                ssh -i '${identity}' -t -o ProxyCommand="ssh -i '${identity}' azureuser@${deployIp} nc ${privateIp} 22" azureuser@${privateIp} "java -jar ${IMAGE_NAME}-${params.TAG_VERSION}.jar"
-              """
-          // echo ${ip}"
-            }
-          }
-	}
-      }
-    }
-
-    stage('Test VMSS') {
-      steps {
-        script {
-          ip = sh(returnStdout: true, script: "az network public-ip show --resource-group $RESOURCE_GROUP --name $IP_NAME --query ipAddress --output tsv").trim()
-          print "Visit http://$ip:$TEST_PORT"
-        }
-      }
-    }
-
-    stage('Switch') {
-      steps {
-        input("Switch Prod Proceed or Abort?")
-				  
+        // Get the terraform plan
         sh """
-	  az network lb rule delete --resource-group $RESOURCE_GROUP --lb-name $LB_NAME --name $TEST_VMSS_NAME
-          az network lb rule update --resource-group $RESOURCE_GROUP --lb-name $LB_NAME --name $PROD_VMSS_NAME --backend-pool-name ${newBackend()}-bepool
-        """
+	  cat \$(cat ${publicKey})
+	"""
       }
     }
-
-    stage('Delete Old VM') {
-      steps {
-        sh """
-	  # Old VM
-	  az vm delete --ids \$(az vm list -g $RESOURCE_GROUP --query "[?contains(name, '${currentBackend}')].id" -o tsv)
-	  az disk delete --ids \$(az disk list -g $RESOURCE_GROUP --query "[?contains(name, '${currentBackend}')].id" -o tsv)
-	  az network nic delete --ids \$(az network nic list -g $RESOURCE_GROUP  --query "[?contains(name, '${currentBackend}')].id" -o tsv)'
 	  
-	  # Jump VM
-	  az vm delete --ids \$(az vm list -g $RESOURCE_GROUP --query "[?contains(name, 'dev')].id" -o tsv)
-	  az disk delete --ids \$(az disk list -g $RESOURCE_GROUP --query "[?contains(name, 'dev')].id" -o tsv)
-	  az network nic delete --ids \$(az network nic list -g $RESOURCE_GROUP  --query "[?contains(name, 'dev')].id" -o tsv)'
-	  
-        """
-      }
-    }
+//     stage('Terraform init'){
+//       steps {
+//         // Initialize the plan
+//         sh  """
+//          cd ${workspace}/${TERRAFORM_PATH}
+//          terraform init -input=false
+//         """
+//       }
+//     }
+// 
+//     stage('Terraform plan'){
+//       steps {
+// 
+//         // Get the VM image ID for the VMSS
+//         withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
+//           sh """
+//             az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
+//             az account set --subscription $AZURE_SUBSCRIPTION_ID
+//            
+//             export ARM_CLIENT_ID="${AZURE_CLIENT_ID}"
+//             export ARM_CLIENT_SECRET="${AZURE_CLIENT_SECRET}"
+//             export ARM_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID}"
+//             export ARM_TENANT_ID="${AZURE_TENANT_ID}"
+//             
+//             cd ${workspace}/${TERRAFORM_PATH}
+//             terraform plan -out=tfplan -input=false \
+//               -var 'app_resource_group_name=${RESOURCE_GROUP}' \
+//               -var 'location=${LOCATION}' \
+//               -var 'prefix=${PREFIX}' \
+//               -var "pool_name=${newBackend()}" \
+//               -var 'vm_instances=2' \
+//               -var "public_key=\$(cat ${publicKey})" \
+//               -var 'client_id=${AZURE_CLIENT_ID}' \
+//               -var 'client_secret=${AZURE_CLIENT_SECRET}' \
+//               -var 'tenant_id=${AZURE_TENANT_ID}' \
+//               -var 'subscription_id=${AZURE_SUBSCRIPTION_ID}'
+//             
+//           """
+//         }
+//         
+//       }
+//     }
+// 
+//     stage('Terraform apply'){
+//       steps {
+//         // Apply the plan
+//         withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
+//           sh  """
+//            cd ${workspace}/${TERRAFORM_PATH}
+//            terraform apply -input=false -auto-approve "tfplan"
+//           """
+//         }
+//       }
+//     }
+// 
+//     stage('APP Image Pull') {
+//       steps {
+//         script {
+//           withCredentials([usernamePassword(credentialsId: NEXUS_CREDENTIALS_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+//             sh """
+//               curl -o ${IMAGE_NAME}-${params.TAG_VERSION}.jar -L -u '${USERNAME}:${PASSWORD}' \\
+//                 -X GET '${REPOSITORY_API}/search/assets/download?repository=${IMAGE_REPOSITORY}&group=${IMAGE_GROUP}&name=${IMAGE_NAME}&version=${params.TAG_VERSION}&maven.extension=jar'
+//               
+//               ls -al
+//             """
+//           }
+//         }
+//       }
+//     }
+// 
+//     stage('APP Deploy') {
+//       steps {
+//         script {
+//           deployIp = sh(returnStdout: true, script: "az network public-ip show -g ${RESOURCE_GROUP} --name vm-dev-pip --query ipAddress --output tsv").trim()
+//           privateIps = sh(returnStdout: true, script: "az network nic list -g ${RESOURCE_GROUP}  --query \"[?contains(name, '${newBackend()}')].ipConfigurations[].privateIpAddress\" -o tsv").split("\n")
+// 
+//           print "deployIp : ${deployIp}"
+//           print "privateIps : ${privateIps}"
+// 		
+//           withCredentials([sshUserPrivateKey(credentialsId: VM_PRIBATE_KEY, keyFileVariable: 'identity', usernameVariable: 'userName')]) {  
+//             for (privateIp in privateIps) {
+//               sh """
+//                 # app push
+//                 scp -i '${identity}' -r -o ProxyCommand="ssh -i '${identity}' -W %h:%p azureuser@${deployIp}" ${IMAGE_NAME}-${params.TAG_VERSION}.zip azureuser@privateIp:~/
+//                 # app run
+//                 ssh -i '${identity}' -t -o ProxyCommand="ssh -i '${identity}' azureuser@${deployIp} nc ${privateIp} 22" azureuser@${privateIp} "java -jar ${IMAGE_NAME}-${params.TAG_VERSION}.jar"
+//               """
+//           // echo ${ip}"
+//             }
+//           }
+// 	}
+//       }
+//     }
+// 
+//     stage('Test VMSS') {
+//       steps {
+//         script {
+//           ip = sh(returnStdout: true, script: "az network public-ip show --resource-group $RESOURCE_GROUP --name $IP_NAME --query ipAddress --output tsv").trim()
+//           print "Visit http://$ip:$TEST_PORT"
+//         }
+//       }
+//     }
+// 
+//     stage('Switch') {
+//       steps {
+//         input("Switch Prod Proceed or Abort?")
+// 				  
+//         sh """
+// 	  az network lb rule delete --resource-group $RESOURCE_GROUP --lb-name $LB_NAME --name $TEST_VMSS_NAME
+//           az network lb rule update --resource-group $RESOURCE_GROUP --lb-name $LB_NAME --name $PROD_VMSS_NAME --backend-pool-name ${newBackend()}-bepool
+//         """
+//       }
+//     }
+// 
+//     stage('Delete Old VM') {
+//       steps {
+//         sh """
+// 	  # Old VM
+// 	  az vm delete --ids \$(az vm list -g $RESOURCE_GROUP --query "[?contains(name, '${currentBackend}')].id" -o tsv)
+// 	  az disk delete --ids \$(az disk list -g $RESOURCE_GROUP --query "[?contains(name, '${currentBackend}')].id" -o tsv)
+// 	  az network nic delete --ids \$(az network nic list -g $RESOURCE_GROUP  --query "[?contains(name, '${currentBackend}')].id" -o tsv)'
+// 	  
+// 	  # Jump VM
+// 	  az vm delete --ids \$(az vm list -g $RESOURCE_GROUP --query "[?contains(name, 'dev')].id" -o tsv)
+// 	  az disk delete --ids \$(az disk list -g $RESOURCE_GROUP --query "[?contains(name, 'dev')].id" -o tsv)
+// 	  az network nic delete --ids \$(az network nic list -g $RESOURCE_GROUP  --query "[?contains(name, 'dev')].id" -o tsv)'
+// 	  
+//         """
+//       }
+//     }
 
     stage('Destroy') {
       steps {
