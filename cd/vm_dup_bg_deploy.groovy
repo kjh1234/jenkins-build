b3d26413e9d866e71d1c7ca52d9c4f65cedd468d
kjh1234 <kjh1234@gmail.com>
2021-04-07 16:14:19 +0900

    Update vm_dup_bg_deploy.groovy

--- cd/vm_dup_bg_deploy.groovy
+++ cd/vm_dup_bg_deploy.groovy
@@ -37,60 +37,60 @@ pipeline {
       }
     }
 	  
-    stage('Terraform init'){
-      steps {
-        // Initialize the plan
-        sh  """
-         cd ${workspace}/${TERRAFORM_PATH}
-         terraform init -input=false
-        """
-      }
-    }
-
-    stage('Terraform plan'){
-      steps {
-
-        // Get the VM image ID for the VMSS
-        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
-          sh """
-            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
-            az account set --subscription $AZURE_SUBSCRIPTION_ID
-           
-            export ARM_CLIENT_ID="${AZURE_CLIENT_ID}"
-            export ARM_CLIENT_SECRET="${AZURE_CLIENT_SECRET}"
-            export ARM_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID}"
-            export ARM_TENANT_ID="${AZURE_TENANT_ID}"
-            
-            cd ${workspace}/${TERRAFORM_PATH}
-            terraform plan -out=tfplan -input=false \
-              -var 'app_resource_group_name=${RESOURCE_GROUP}' \
-              -var 'location=${LOCATION}' \
-              -var 'prefix=${PREFIX}' \
-              -var "pool_name=${newBackend()}" \
-              -var 'vm_instances=2' \
-              -var "public_key=\$(cat ${publicKey})" \
-              -var 'client_id=${AZURE_CLIENT_ID}' \
-              -var 'client_secret=${AZURE_CLIENT_SECRET}' \
-              -var 'tenant_id=${AZURE_TENANT_ID}' \
-              -var 'subscription_id=${AZURE_SUBSCRIPTION_ID}'
-            
-          """
-        }
-        
-      }
-    }
-
-    stage('Terraform apply'){
-      steps {
-        // Apply the plan
-        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
-          sh  """
-           cd ${workspace}/${TERRAFORM_PATH}
-           terraform apply -input=false -auto-approve "tfplan"
-          """
-        }
-      }
-    }
+//    stage('Terraform init'){
+//      steps {
+//        // Initialize the plan
+//        sh  """
+//         cd ${workspace}/${TERRAFORM_PATH}
+//         terraform init -input=false
+//        """
+//      }
+//    }
+//
+//    stage('Terraform plan'){
+//      steps {
+//
+//        // Get the VM image ID for the VMSS
+//        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
+//          sh """
+//            az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET -t $AZURE_TENANT_ID
+//            az account set --subscription $AZURE_SUBSCRIPTION_ID
+//           
+//            export ARM_CLIENT_ID="${AZURE_CLIENT_ID}"
+//            export ARM_CLIENT_SECRET="${AZURE_CLIENT_SECRET}"
+//            export ARM_SUBSCRIPTION_ID="${AZURE_SUBSCRIPTION_ID}"
+//            export ARM_TENANT_ID="${AZURE_TENANT_ID}"
+//            
+//            cd ${workspace}/${TERRAFORM_PATH}
+//            terraform plan -out=tfplan -input=false \
+//              -var 'app_resource_group_name=${RESOURCE_GROUP}' \
+//              -var 'location=${LOCATION}' \
+//              -var 'prefix=${PREFIX}' \
+//              -var "pool_name=${newBackend()}" \
+//              -var 'vm_instances=2' \
+//              -var "public_key=\$(cat ${publicKey})" \
+//              -var 'client_id=${AZURE_CLIENT_ID}' \
+//              -var 'client_secret=${AZURE_CLIENT_SECRET}' \
+//              -var 'tenant_id=${AZURE_TENANT_ID}' \
+//              -var 'subscription_id=${AZURE_SUBSCRIPTION_ID}'
+//            
+//          """
+//        }
+//        
+//      }
+//    }
+//
+//    stage('Terraform apply'){
+//      steps {
+//        // Apply the plan
+//        withCredentials([azureServicePrincipal(INNO_AZURE_CREDENTIALS)]) {
+//          sh  """
+//           cd ${workspace}/${TERRAFORM_PATH}
+//           terraform apply -input=false -auto-approve "tfplan"
+//          """
+//        }
+//      }
+//    }
 
     stage('APP Image Pull') {
       steps {
