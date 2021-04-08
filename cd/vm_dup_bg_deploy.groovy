70e83c07f070268d7aeb12b1b79a288742e151b2
kjh1234 <kjh1234@gmail.com>
2021-04-08 10:39:56 +0900

    Update vm_dup_bg_deploy.groovy

--- cd/vm_dup_bg_deploy.groovy
+++ cd/vm_dup_bg_deploy.groovy
@@ -127,9 +127,11 @@ pipeline {
             for (privateIp in privateIps) {
               sh """
                 # app push
-                scp -i '${identity}' -r -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i '${identity}' -o StrictHostKeyChecking=no -W %h:%p azureuser@${deployIp}" ${IMAGE_NAME}-${params.TAG_VERSION}.jar azureuser@${privateIp}:~/
+                scp -i '${identity}' -r -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i '${identity}' -o StrictHostKeyChecking=no -W %h:%p azureuser@${deployIp}" \\
+		  ${IMAGE_NAME}-${params.TAG_VERSION}.jar azureuser@${privateIp}:~/
                 # app run
-                ssh -i '${identity}' -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i '${identity}' -o StrictHostKeyChecking=no azureuser@${deployIp} nc ${privateIp} 22" azureuser@${privateIp} "java -jar ${IMAGE_NAME}-${params.TAG_VERSION}.jar &"
+                ssh -i '${identity}' -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i '${identity}' -o StrictHostKeyChecking=no azureuser@${deployIp} nc ${privateIp} 22" \\
+		  azureuser@${privateIp} "nohup java -jar ${IMAGE_NAME}-${params.TAG_VERSION}.jar &"
               """
           // echo ${ip}"
             }
