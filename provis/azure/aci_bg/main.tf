478c6bfa5f2c54e1956c836f9d55f9e82361c744
kjh1234 <kjh1234@gmail.com>
2021-04-09 17:56:23 +0900

    Update main.tf

--- provis/azure/aci_bg/main.tf
+++ provis/azure/aci_bg/main.tf
@@ -16,14 +16,23 @@ resource "azurerm_virtual_network" "main" {
   name                = "aci-vnet"
   resource_group_name = "${azurerm_resource_group.main.name}"
   location            = "${azurerm_resource_group.main.location}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "main" {
   name                 = "aci-subnet"
   virtual_network_name = "${azurerm_virtual_network.main.name}"
   resource_group_name  = "${azurerm_resource_group.main.name}"
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.1.0.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
 
 resource "azurerm_network_profile" "main" {
