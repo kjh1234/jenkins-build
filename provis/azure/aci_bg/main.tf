7183c23cf79705de0634cd319ce98591e1b40cee
권준호 SKC&C/SK텔레콤_유관부서 <yamanin1@partner.sk.com>
2021-04-12 17:50:20 +0900

    ACI 구성

--- provis/azure/aci_bg/main.tf
+++ provis/azure/aci_bg/main.tf
@@ -35,23 +35,75 @@ resource "azurerm_subnet" "main" {
   }
 }
 
+# Create public IPs
+resource "azurerm_public_ip" "main" {
+  name                 = "${var.prefix}-pip"
+  location             = "${var.location}"
+  resource_group_name  = "${var.app_resource_group_name}"
+  allocation_method            = "Static"
+  sku                          = "standard"
+}
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = "${var.location}"
+  resource_group_name = "${var.app_resource_group_name}"
+  sku                 = "standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = "${azurerm_public_ip.main.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = "${var.app_resource_group_name}"
+  loadbalancer_id     = "${var.lb_id}"
+  name                = "blue-bepool"
+}
+
+resource "azurerm_lb_probe" "main" {
+  resource_group_name = "${var.app_resource_group_name}"
+  loadbalancer_id     = "${var.lb_id}"
+  name                = "prod-probe"
+  port                = "${var.application_port}"
+}
+
+resource "azurerm_lb_rule" "main" {
+  resource_group_name            = "${var.app_resource_group_name}"
+  loadbalancer_id                = "${var.lb_id}"
+  name                           = "prod-rule"
+  protocol                       = "Tcp"
+  frontend_port                  = "${var.frontend_port}"
+  backend_port                   = "${var.application_port}"
+  backend_address_pool_id        = "${var.lb_backend_address_pool_id}"
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = "${var.lb_probe_id}"
+}
+
 resource "azurerm_network_profile" "main" {
-  name                = "aci-net-profile"
+  name                = "${var.prefix}-blue-net-profile"
   resource_group_name = "${azurerm_resource_group.main.name}"
   location            = "${azurerm_resource_group.main.location}"
 
   container_network_interface {
-    name = "aci-blue-nic"
+    name = "${var.prefix}-blue-nic"
 
     ip_configuration {
-      name      = "exampleipconfig"
+      name      = "${var.prefix}-blue-nic-config"
       subnet_id = "${azurerm_subnet.main.id}"
     }
   }
 }
 
+resource "azurerm_network_interface_backend_address_pool_association" "example" {
+  network_interface_id    = "${azurerm_network_profile.main.container_network_interface.id}"
+  ip_configuration_name   = "${var.prefix}-blue-nic-config"
+  backend_address_pool_id = "${azurerm_lb_backend_address_pool.main.id}"
+}
+
 resource "azurerm_container_group" "main" {
-  name                = "${var.prefix}-ctn-grp"
+  name                = "${var.prefix}-blue-ci"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
   ip_address_type     = "Private"
@@ -65,7 +117,7 @@ resource "azurerm_container_group" "main" {
   }
 
   container {
-    name   = "hello-world"
+    name   = "todo-app"
     image  = "innoregi.azurecr.io/todo-app:1.0.0"
     cpu    = "0.5"
     memory = "1.5"
