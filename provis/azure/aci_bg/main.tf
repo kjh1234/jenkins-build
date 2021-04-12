96a46d2cbfc0580c376c090ecd4e47ce9f7d3f18
kjh1234 <kjh1234@gmail.com>
2021-04-12 17:57:25 +0900

    Update main.tf

--- provis/azure/aci_bg/main.tf
+++ provis/azure/aci_bg/main.tf
@@ -58,25 +58,25 @@ resource "azurerm_lb" "main" {
 
 resource "azurerm_lb_backend_address_pool" "main" {
   resource_group_name = "${var.app_resource_group_name}"
-  loadbalancer_id     = "${var.lb_id}"
+  loadbalancer_id     = "${azurerm_lb.main.id}"
   name                = "blue-bepool"
 }
 
 resource "azurerm_lb_probe" "main" {
   resource_group_name = "${var.app_resource_group_name}"
-  loadbalancer_id     = "${var.lb_id}"
+  loadbalancer_id     = "${azurerm_lb.main.id}"
   name                = "prod-probe"
   port                = "${var.application_port}"
 }
 
 resource "azurerm_lb_rule" "main" {
   resource_group_name            = "${var.app_resource_group_name}"
-  loadbalancer_id                = "${var.lb_id}"
+  loadbalancer_id                = "${azurerm_lb.main.id}"
   name                           = "prod-rule"
   protocol                       = "Tcp"
   frontend_port                  = "${var.frontend_port}"
   backend_port                   = "${var.application_port}"
-  backend_address_pool_id        = "${var.lb_backend_address_pool_id}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.main.id}"
   frontend_ip_configuration_name = "PublicIPAddress"
   probe_id                       = "${var.lb_probe_id}"
 }
