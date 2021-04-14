cb982cb41973cb423c7e5d787325c0b9211b9f72
kjh1234 <kjh1234@gmail.com>
2021-04-14 11:28:18 +0900

    Update aci_bg_build.groovy

--- ci/aci_bg_build.groovy
+++ ci/aci_bg_build.groovy
@@ -12,34 +12,33 @@ pipeline {
 
     stage('SCM') {
       steps {
-          checkout([
-              $class: 'GitSCM',
-              branches: [[name: "refs/tags/${TAG_VERSION}"]],
-              doGenerateSubmoduleConfigurations: false,
-              extensions: [[
-                  $class: 'SubmoduleOption',
-                  disableSubmodules: false,
-                  parentCredentials: false,
-                  recursiveSubmodules: false,
-                  reference: '',
-                  trackingSubmodules: false
-                ],[
-                  $class: 'RelativeTargetDirectory',
-                relativeTargetDir: "${workspace}/tmp_source"
-              ]],
-              submoduleCfg: [],
-              userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/todo-app-java-on-azure.git"]]
-          ])
+        /* 빌드 대상 GIT을 임시 폴더에 생성함 */
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: "refs/tags/${TAG_VERSION}"]],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[
+                $class: 'SubmoduleOption',
+                disableSubmodules: false,
+                parentCredentials: false,
+                recursiveSubmodules: false,
+                reference: '',
+                trackingSubmodules: false
+              ],[
+                $class: 'RelativeTargetDirectory',
+              relativeTargetDir: "${workspace}/tmp_source"
+            ]],
+            submoduleCfg: [],
+            userRemoteConfigs: [[credentialsId: GIT_CREDENTIALS_ID, url: "https://github.com/kjh1234/todo-app-java-on-azure.git"]]
+        ])
       }
     }
 
     stage('BUILD') {
       steps {
+        /* 임시 폴더를 이용하여 빌드시 매 Jenkins 컴포넌트구동시 WorkSpace에서 시작하기 때문에 위치를 설정해주어야함. */
         sh """
           cd ${workspace}/tmp_source
-        """
-        sh """
-          pwd
           sh ./mvnw clean package -Dmaven.test.skip=true
         """
       }
@@ -48,11 +47,11 @@ pipeline {
     stage('Create/Push Docker Image') {
       steps {
         withDockerRegistry([credentialsId: DOCKER_CREDENTIALS_ID, url: DOCKER_REGISTRY_URL]) {
-            sh """
-              pwd
-              docker build -t "${env.IMAGE_TAG}" .
-              docker push "${env.IMAGE_TAG}"
-            """
+          sh """
+            cd ${workspace}/tmp_source
+            docker build -t "${env.IMAGE_TAG}" .
+            docker push "${env.IMAGE_TAG}"
+          """
         }
       }
     }
