dff7cbd14b77a849fd79da13bf4f689825c8347e
권준호 SKC&C/SK텔레콤_유관부서 <yamanin1@partner.sk.com>
2021-04-15 14:06:51 +0900

    AWS 샘플

---
+++ provis/aws/vm_sample/main.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.location}"
+}
+
+data "aws_vpc" "main" {
+  tags {
+    Name = "${var.vpc}"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "main" {
+  key_name = "test-key1"
+  public_key = "${var.public_key}"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "${var.subnet_cidr}"
+  availability_zone = "${var.location}"
+  tags {
+    Name = "${var.prefix}_subnet"
+    group = "${var.app_resource_group_name}"
+  }
+}
+
+resource "aws_security_group" "main" {
+  name = "${var.prefix}_nsg"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    group = "${var.app_resource_group_name}"
+  }
+}
+
+resource "aws_instance" "main" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.main.id}"
+  vpc_security_group_ids = ["${aws_security_group.main.id}"]
+  key_name = "${aws_key_pair.main.key_name}"
+  count = 1
+  tags {
+    Name = "${var.prefix}_ec2"
+    group = "${var.app_resource_group_name}"
+  }
+}
---
+++ provis/aws/vm_sample/variables.tf
@@ -0,0 +1,45 @@
+#     Credential variables     #
+variable "access_key" {
+  description = "Should be on the credentials file if not you must generate it."
+}
+
+variable "secret_key" {
+  description = "Should be on the credentials file if not you must generate it."
+}
+
+#     resource variables     #
+variable "app_resource_group_name" {
+  description = "Resource Group name, must be lowercase alphanumeric with hyphens as its used as domain_name_label as well"
+  default     = "test_vm"
+}
+
+variable "location" {
+  description = "The location where resources are created"
+  default     = "ap-northeast-2"
+}
+
+variable "prefix" {
+  default     = "test_vm"
+}
+
+variable "vpc-name" {
+  default     = "doss-vpc"
+}
+
+variable "public_key" {
+    default = ""
+}
+
+variable "subnet_cidr" {
+    default = "10.0.3.0/24"
+}
+
+variable "frontend_port" {
+    description = "The frontend port of the external Load Balancer"
+    default     = 80
+}
+
+variable "application_port" {
+    description = "The backend port where the application can be accessed"
+    default     = 8080
+}
