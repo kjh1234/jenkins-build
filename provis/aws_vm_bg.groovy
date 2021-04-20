5fcdbd7d15cf60990f56b1c593dc05f9b895bd94
권준호 SKC&C/SK텔레콤_유관부서 <yamanin1@partner.sk.com>
2021-04-20 16:09:26 +0900

    AWS BG 데모

---
+++ provis/aws_vm_bg.groovy
@@ -0,0 +1,56 @@
+pipeline {
+  agent any
+  stages {
+    stage('Checkout'){
+      steps {
+        // Get the terraform plan
+        checkout scm
+      }
+    }
+    stage('Terraform init'){
+      steps {
+        // Initialize the plan
+        sh  """
+         cd ${workspace}/${TERRAFORM_PATH}
+         terraform init -input=false
+        """
+      }
+    }
+    stage('Terraform plan'){
+      steps {
+
+        // Get the VM image ID for the VMSS
+        withCredentials([usernamePassword(credentialsId: AWS_ACCOUNT, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+          sh """
+
+            cd ${workspace}/${TERRAFORM_PATH}
+            terraform plan -out=tfplan -input=false \
+              -var 'app_resource_group_name=${RESOURCE_GROUP}' \
+              -var "public_key=\$(cat ${PUBLIC_KEY})" \
+              -var 'access_key=${USERNAME}' \
+              -var 'secret_key=${PASSWORD}' \
+              -var 'location=${LOCATION}' \
+
+          """
+        }
+      }
+    }
+
+    stage('Terraform apply'){
+      steps {
+        // Apply the plan
+        sh  """
+         cd ${workspace}/${TERRAFORM_PATH}
+         terraform apply -input=false -auto-approve "tfplan"
+        """
+      }
+    }
+  }
+  environment {
+    AWS_ACCOUNT = "AWS_P120230_ACCOUNT"
+    PUBLIC_KEY="~/.ssh/inno_id_rsa2.pub"
+    RESOURCE_GROUP="vm-bg-gr"
+    LOCATION="ap-northeast-2"
+    TERRAFORM_PATH="provis/aws/vm_bg"
+  }
+}
