08358a05c033439b0dc760ecebd227bf402f60a7
kjh1234 <kjh1234@gmail.com>
2021-04-20 17:48:52 +0900

    Update main.tf

--- provis/aws/vm_bg/main.tf
+++ provis/aws/vm_bg/main.tf
@@ -147,28 +147,6 @@ resource "aws_lb" "main" {
     subnet_id     = "${aws_subnet.green.id}"
   }
 
-  listener {
-    instance_port     = 8080
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  listener {
-    instance_port     = 8080
-    instance_protocol = "http"
-    lb_port           = 8080
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
   tags = {
     Name = "${var.prefix}-alb"
     group = "${var.app_resource_group_name}"
