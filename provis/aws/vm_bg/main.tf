6f8cebc3c7d9b826d8021aa4e76b885c78f64c22
kjh1234 <kjh1234@gmail.com>
2021-04-20 18:25:43 +0900

    Update main.tf

--- provis/aws/vm_bg/main.tf
+++ provis/aws/vm_bg/main.tf
@@ -214,23 +214,23 @@ resource "aws_lb_target_group_attachment" "stage" {
 }
 
 resource "aws_lb_listener" "prod" {
-  load_balancer_arn = aws_lb.front_end.arn
+  load_balancer_arn = "${aws_lb.main.arn}"
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.prod.arn
+    target_group_arn = "${aws_lb_target_group.prod.arn}"
   }
 }
 
 resource "aws_lb_listener" "stage" {
-  load_balancer_arn = aws_lb.front_end.arn
+  load_balancer_arn = "${aws_lb.main.arn}"
   port              = "8080"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.stage.arn
+    target_group_arn = "${aws_lb_target_group.stage.arn}"
   }
 }
