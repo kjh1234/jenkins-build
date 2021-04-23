f0032f211c32c48b77ac67f44243b3fec92170ae
권준호 SKC&C/SK텔레콤_유관부서 <yamanin1@partner.sk.com>
2021-04-23 14:07:48 +0900

    AWS VMSS 추가

---
+++ provis/aws/vmss_bg/main.tf
@@ -0,0 +1,180 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.location}"
+}
+
+
+data "aws_vpc" "main" {
+//  cidr_block       = "172.31.128.0/20"
+  most_recent = true
+  filter {
+    name = "Name"
+    values = ["vmss-bg-subnet-blue"]
+  }
+}
+
+
+data "aws_subnet" "green" {
+//  cidr_block       = "172.31.144.0/20"
+  most_recent = true
+  filter {
+    name = "Name"
+    values = ["vmss-bg-subnet-green"]
+  }
+}
+
+
+data "aws_security_group" "main" {
+//  cidr_block       = "172.31.144.0/20"
+  most_recent = true
+  filter {
+    name = "Name"
+    values = ["vmss-bg-nsg"]
+  }
+}
+
+
+resource "aws_lb" "main" {
+  name               = "${var.prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = ["${data.aws_security_group.main.id}"]
+  subnets = ["${data.aws_subnet.blue.id}", "${data.aws_subnet.green.id}"]
+//  enable_deletion_protection = true
+
+  tags = {
+    Name = "${var.prefix}-alb"
+    group = "${var.app_resource_group_name}"
+  }
+}
+
+resource "aws_launch_template" "blue" {
+  name_prefix   = "todo-app-1.0.0"
+  image_id      = "ami-0ad206874e2824fce"
+  instance_type = "t2.micro"
+}
+
+resource "aws_launch_template" "green" {
+  name_prefix   = "todo-app-1.0.1"
+  image_id      = "ami-00293a4b4544ef9bc"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "blue" {
+  availability_zones = ["ap-northeast-2a"]
+  desired_capacity   = 2
+  min_size           = 2
+  max_size           = 5
+
+  launch_template {
+    id      = aws_launch_template.blue.id
+  }
+}
+
+resource "aws_autoscaling_group" "green" {
+  availability_zones = ["ap-northeast-2c"]
+  desired_capacity   = 2
+  min_size           = 2
+  max_size           = 5
+
+  launch_template {
+    id      = aws_launch_template.green.id
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "${var.prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = ["${data.aws_security_group.main.id}"]
+  subnets = ["${data.aws_subnet.blue.id}", "${data.aws_subnet.green.id}"]
+//  enable_deletion_protection = true
+
+  tags = {
+    Name = "${var.prefix}-alb"
+    group = "${var.app_resource_group_name}"
+  }
+}
+
+resource "aws_lb_target_group" "prod" {
+  name   = "${var.prefix}-lb-prod-target"
+  vpc_id = "${data.aws_vpc.main.id}"
+  port = "80"
+  protocol = "HTTP"
+  target_type = "instance"
+  deregistration_delay = "300"
+  slow_start = "0"
+  health_check {
+    path = "/"
+    healthy_threshold = "5"
+    unhealthy_threshold = "2"
+    timeout = "5"
+    interval = "30"
+    matcher = "200"
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+
+  tags = {
+    group = "${var.app_resource_group_name}"
+  }
+}
+
+resource "aws_lb_target_group" "stage" {
+  name   = "${var.prefix}-lb-stage-target"
+  vpc_id = "${data.aws_vpc.main.id}"
+  port = "8080"
+  protocol = "HTTP"
+  target_type = "instance"
+  deregistration_delay = "300"
+  slow_start = "0"
+  health_check {
+    path = "/"
+    healthy_threshold = "5"
+    unhealthy_threshold = "2"
+    timeout = "5"
+    interval = "30"
+    matcher = "200"
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+
+  tags = {
+    group = "${var.app_resource_group_name}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "prod" {
+  target_group_arn = "${aws_lb_target_group.prod.arn}"
+  target_id        = "${aws_autoscaling_group.blue.id}"
+  port             = 8080
+}
+
+resource "aws_lb_target_group_attachment" "stage" {
+  target_group_arn = "${aws_lb_target_group.stage.arn}"
+  target_id        = "${aws_autoscaling_group.green.id}"
+  port             = 8080
+}
+
+resource "aws_lb_listener" "prod" {
+  load_balancer_arn = "${aws_lb.main.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.prod.arn}"
+  }
+}
+
+resource "aws_lb_listener" "stage" {
+  load_balancer_arn = "${aws_lb.main.arn}"
+  port              = "8080"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.stage.arn}"
+  }
+}
