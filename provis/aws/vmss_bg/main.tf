bf3a4277c7dd5c6ea267b7d31a3dbd9b1a9c76c4
kjh1234 <kjh1234@gmail.com>
2021-04-23 21:09:31 +0900

    Update main.tf

--- provis/aws/vmss_bg/main.tf
+++ provis/aws/vmss_bg/main.tf
@@ -134,7 +134,7 @@ resource "aws_lb_target_group" "prod" {
 resource "aws_lb_target_group" "stage" {
   name   = "${var.prefix}-lb-stage-target"
   vpc_id = "${data.aws_vpc.main.id}"
-  port = "8080"
+  port = "80"
   protocol = "HTTP"
   target_type = "instance"
   deregistration_delay = "300"
