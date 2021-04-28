d84c026ae3dad3a11592258b962c0064cd9bbc55
권준호 SKC&C/SK텔레콤_유관부서 <yamanin1@partner.sk.com>
2021-04-28 10:35:12 +0900

    Blue만 생성 하도록

--- provis/aws/vm_dup_bg/alb.tf
+++ provis/aws/vm_dup_bg/alb.tf
@@ -37,30 +37,30 @@ resource "aws_lb_target_group" "blue" {
   }
 }
 
-resource "aws_lb_target_group" "green" {
-  name   = "${var.prefix}-lb-green-target"
-  vpc_id = "${data.aws_vpc.main.id}"
-  port = "8080"
-  protocol = "HTTP"
-  target_type = "instance"
-  deregistration_delay = "300"
-  slow_start = "0"
-  health_check {
-    path = "/"
-    healthy_threshold = "5"
-    unhealthy_threshold = "2"
-    timeout = "5"
-    interval = "30"
-    matcher = "200"
-    port = "traffic-port"
-    protocol = "HTTP"
-  }
-
-  tags = {
-    group = "${var.app_resource_group_name}"
-    pool_name = "blue"
-  }
-}
+//resource "aws_lb_target_group" "green" {
+//  name   = "${var.prefix}-lb-green-target"
+//  vpc_id = "${data.aws_vpc.main.id}"
+//  port = "8080"
+//  protocol = "HTTP"
+//  target_type = "instance"
+//  deregistration_delay = "300"
+//  slow_start = "0"
+//  health_check {
+//    path = "/"
+//    healthy_threshold = "5"
+//    unhealthy_threshold = "2"
+//    timeout = "5"
+//    interval = "30"
+//    matcher = "200"
+//    port = "traffic-port"
+//    protocol = "HTTP"
+//  }
+//
+//  tags = {
+//    group = "${var.app_resource_group_name}"
+//    pool_name = "blue"
+//  }
+//}
 
 resource "aws_lb_listener" "prod" {
   load_balancer_arn = "${aws_lb.main.arn}"
@@ -73,13 +73,13 @@ resource "aws_lb_listener" "prod" {
   }
 }
 
-resource "aws_lb_listener" "stage" {
-  load_balancer_arn = "${aws_lb.main.arn}"
-  port              = "8080"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.green.arn}"
-  }
-}
+//resource "aws_lb_listener" "stage" {
+//  load_balancer_arn = "${aws_lb.main.arn}"
+//  port              = "8080"
+//  protocol          = "HTTP"
+//
+//  default_action {
+//    type             = "forward"
+//    target_group_arn = "${aws_lb_target_group.green.arn}"
+//  }
+//}
--- provis/aws/vm_dup_bg/instance.tf
+++ provis/aws/vm_dup_bg/instance.tf
@@ -13,20 +13,20 @@ resource "aws_instance" "blue" {
   }
 }
 
-resource "aws_instance" "green" {
-  count = 2
-
-  instance_type          = "t2.micro"
-  subnet_id              = "${data.aws_subnet.green.id}"
-  vpc_security_group_ids = ["${data.aws_security_group.main.id}"]
-  key_name               = "test-key1"
-  ami = "ami-05e9bd7595a88caf3"
-
-  tags = {
-    Name = "${var.prefix}-ec2-green-${count.index}"
-    group = "${var.app_resource_group_name}"
-  }
-}
+//resource "aws_instance" "green" {
+//  count = 2
+//
+//  instance_type          = "t2.micro"
+//  subnet_id              = "${data.aws_subnet.green.id}"
+//  vpc_security_group_ids = ["${data.aws_security_group.main.id}"]
+//  key_name               = "test-key1"
+//  ami = "ami-05e9bd7595a88caf3"
+//
+//  tags = {
+//    Name = "${var.prefix}-ec2-green-${count.index}"
+//    group = "${var.app_resource_group_name}"
+//  }
+//}
 
 resource "aws_lb_target_group_attachment" "blue" {
   count = 2
@@ -36,10 +36,10 @@ resource "aws_lb_target_group_attachment" "blue" {
   port             = 8080
 }
 
-resource "aws_lb_target_group_attachment" "green" {
-  count = 2
-
-  target_group_arn = "${aws_lb_target_group.green.arn}"
-  target_id        = "${element(aws_instance.green, count.index).id}"
-  port             = 8080
-}
+//resource "aws_lb_target_group_attachment" "green" {
+//  count = 2
+//
+//  target_group_arn = "${aws_lb_target_group.green.arn}"
+//  target_id        = "${element(aws_instance.green, count.index).id}"
+//  port             = 8080
+//}
