9419c35f0bf1a110610697394331659881021b89
kjh1234 <kjh1234@gmail.com>
2021-04-29 16:05:44 +0900

    Update network.tf

--- provis/aws/vmss_bg/network.tf
+++ provis/aws/vmss_bg/network.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.location}"
-}
-
 data "aws_vpc" "main" {
   cidr_block       = "172.31.0.0/16"
 }
