19d534bf873e6ee5b9a57aebbbc92bb914635a07
P120230 <P120230@SKTP120230PN006.SKT.AD>
2021-04-29 17:45:11 +0900

    수정

--- provis/aws/vmss_bg/alb.tf
+++ provis/aws/vmss_bg/alb.tf
@@ -23,43 +23,8 @@ resource "aws_lb_listener" "prod" {
   }
 }
 
-resource "aws_lb_listener" "stage" {
-  load_balancer_arn = "${aws_lb.main.arn}"
-  port              = "8080"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_lb_target_group.green.arn}"
-  }
-}
-
 resource "aws_lb_target_group" "blue" {
   name   = "${var.prefix}-lb-blue-target"
-  vpc_id = "${data.aws_vpc.main.id}"
-  port = "8080"
-  protocol = "HTTP"
-  target_type = "instance"
-  deregistration_delay = "300"
-  slow_start = "0"
-  health_check {
-    path = "/"
-    healthy_threshold = "5"
-    unhealthy_threshold = "2"
-    timeout = "5"
-    interval = "30"
-    matcher = "200"
-    port = "traffic-port"
-    protocol = "HTTP"
-  }
-
-  tags = {
-    group = "${var.app_resource_group_name}"
-  }
-}
-
-resource "aws_lb_target_group" "green" {
-  name   = "${var.prefix}-lb-green-target"
   vpc_id = "${data.aws_vpc.main.id}"
   port = "8080"
   protocol = "HTTP"
--- provis/aws/vmss_bg/instances.tf
+++ provis/aws/vmss_bg/instances.tf
@@ -9,17 +9,6 @@ resource "aws_launch_template" "blue" {
   }
 }
 
-resource "aws_launch_template" "green" {
-  name_prefix   = "todo-app-1.0.1"
-  image_id      = "ami-05e9bd7595a88caf3"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${data.aws_security_group.main.id}"]
-
-  tags = {
-    group = "${var.app_resource_group_name}"
-  }
-}
-
 resource "aws_autoscaling_group" "blue" {
   availability_zones = ["ap-northeast-2a"]
   desired_capacity   = 2
@@ -44,36 +33,7 @@ resource "aws_autoscaling_group" "blue" {
   ]
 }
 
-resource "aws_autoscaling_group" "green" {
-  availability_zones = ["ap-northeast-2c"]
-  desired_capacity   = 2
-  min_size           = 2
-  max_size           = 5
-
-  launch_template {
-    id      = aws_launch_template.green.id
-  }
-
-  tags = [
-    {
-      "key"                 = "group"
-      "value"               = "${var.app_resource_group_name}"
-      "propagate_at_launch" = true
-    },
-    {
-      "key"                 = "Name"
-      "value"               = "${var.prefix}-green"
-      "propagate_at_launch" = true
-    }
-  ]
-}
-
 resource "aws_autoscaling_attachment" "blue" {
   autoscaling_group_name = "${aws_autoscaling_group.blue.id}"
   alb_target_group_arn   = "${aws_lb_target_group.blue.arn}"
-}
-
-resource "aws_autoscaling_attachment" "green" {
-  autoscaling_group_name = "${aws_autoscaling_group.green.id}"
-  alb_target_group_arn   = "${aws_lb_target_group.green.arn}"
 }
