ee2d694e05d1fd81cf642523728edba67175418f
kjh1234 <kjh1234@gmail.com>
2021-04-30 13:49:16 +0900

    Update aws_vm_dup_bg_deploy.groovy

--- cd/aws_vm_dup_bg_deploy.groovy
+++ cd/aws_vm_dup_bg_deploy.groovy
@@ -118,7 +118,7 @@ pipeline {
 		  ${IMAGE_NAME}-${params.TAG_VERSION}.jar ubuntu@${privateIp}:~/
                 # app run
                 ssh -i '${identity}' -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i '${identity}' -o StrictHostKeyChecking=no ubuntu@${deployIp} nc ${privateIp} 22" \\
-		  ubuntu@${privateIp} "java -jar ${IMAGE_NAME}-${params.TAG_VERSION}.jar &>/dev/null &"
+		  ubuntu@${privateIp} "java -jar ${IMAGE_NAME}-${params.TAG_VERSION}.jar server.port=8080 &>/dev/null &"
               """
           // echo ${ip}"
             }
