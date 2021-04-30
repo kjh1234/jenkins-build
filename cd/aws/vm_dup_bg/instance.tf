af591af425aa6247d91f438571d954edc7139c98
kjh1234 <kjh1234@gmail.com>
2021-04-30 15:31:41 +0900

    Update instance.tf

--- cd/aws/vm_dup_bg/instance.tf
+++ cd/aws/vm_dup_bg/instance.tf
@@ -11,7 +11,8 @@ data "aws_ami" "ubuntu" {
     most_recent = true
     filter {
         name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
     }
     filter {
         name = "virtualization-type"
